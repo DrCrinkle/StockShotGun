319509949f46be0370725e60c12877dc35795726
Taylor Asplund <62564740+DrCrinkle@users.noreply.github.com>
2024-02-21 19:20:03 -0800

    remove empty try excepts

--- brokers.py
+++ brokers.py
@@ -18,34 +18,29 @@ async def alpacaTrade(side, qty, ticker, price):
     
     trading_client = TradingClient(ALPACA_ACCESS_KEY_ID, ALPACA_SECRET_ACCESS_KEY, paper=True)
 
-    try:
-        if price is not None:
-            limit_order_data = LimitOrderRequest(
-                                    symbol=ticker,
-                                    limit_price=price,
-                                    qty=qty,
-                                    side=side,
-                                    time_in_force='day')
-            trading_client.submit_order(order_data=limit_order_data)
-            if side == "buy":
-                print(f"Bought {ticker} on Alpaca")
-            else:
-                print(f"Sold {ticker} on Alpaca")
+    if price is not None:
+        limit_order_data = LimitOrderRequest(
+                                symbol=ticker,
+                                limit_price=price,
+                                qty=qty,
+                                side=side,
+                                time_in_force='day')
+        trading_client.submit_order(order_data=limit_order_data)
+        if side == "buy":
+            print(f"Bought {ticker} on Alpaca")
         else:
-            market_order_data = MarketOrderRequest(
-                                    symbol=ticker,
-                                    qty=qty,
-                                    side=side,
-                                    time_in_force='day')
-            trading_client.submit_order(order_data=market_order_data)
-            if side == "buy":
-                print(f"Bought {ticker} on Alpaca")
-            else:
-                print(f"Sold {ticker} on Alpaca")
-    except:
-        return False
-
-    return True
+            print(f"Sold {ticker} on Alpaca")
+    else:
+        market_order_data = MarketOrderRequest(
+                                symbol=ticker,
+                                qty=qty,
+                                side=side,
+                                time_in_force='day')
+        trading_client.submit_order(order_data=market_order_data)
+        if side == "buy":
+            print(f"Bought {ticker} on Alpaca")
+        else:
+            print(f"Sold {ticker} on Alpaca")
 
 
 async def robinTrade(side, qty, ticker, price):
@@ -61,22 +56,18 @@ async def robinTrade(side, qty, ticker, price):
     mfa = pyotp.TOTP(ROBINHOOD_MFA).now()
     rh.login(ROBINHOOD_USER, ROBINHOOD_PASS, mfa_code=mfa)
 
-    try:
-        if side == 'buy':
-            if price is not None:
-                rh.order_buy_limit(symbol=ticker, quantity=qty, limitPrice=price)
-            else:
-                rh.order_buy_market(symbol=ticker, quantity=qty)
-            print(f"Bought {ticker} on Robinhood")
+    if side == 'buy':
+        if price is not None:
+            rh.order_buy_limit(symbol=ticker, quantity=qty, limitPrice=price)
         else:
-            if price is not None:
-                rh.order_sell_limit(symbol=ticker, quantity=qty, limitPrice=price)
-            else:
-                rh.order_sell_market(symbol=ticker, quantity=qty)
-            print(f"Sold {ticker} on Robinhood")
-    except:
-        return False
-    return True
+            rh.order_buy_market(symbol=ticker, quantity=qty)
+        print(f"Bought {ticker} on Robinhood")
+    else:
+        if price is not None:
+            rh.order_sell_limit(symbol=ticker, quantity=qty, limitPrice=price)
+        else:
+            rh.order_sell_market(symbol=ticker, quantity=qty)
+        print(f"Sold {ticker} on Robinhood")
 
 
 async def tradierTrade(side, qty, ticker, price):
@@ -101,44 +92,39 @@ async def tradierTrade(side, qty, ticker, price):
         return False
 
 
-    try:
-        if price is not None:
-            for i in range(len(TRADIER_ACCOUNT_ID)):
-                response = requests.post(f'https://api.tradier.com/v1/accounts/{TRADIER_ACCOUNT_ID[i]}/orders',
-                                        data = {'class': 'equity',
-                                                'symbol': f'{ticker}',
-                                                'side': f'{side}',
-                                                'quantity': f'{qty}',
-                                                'type': 'limit',
-                                                'duration': 'day',
-                                                'price': f'{price}'},
-                                        headers = {'Authorization': f'Bearer {TRADIER_ACCESS_TOKEN}',
-                                                  'Accept': 'application/json'}
-                                        )
-                if side == "buy":
-                    print(f"Bought {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
-                else:
-                    print(f"Sold {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
-        else:
-            for i in range(len(TRADIER_ACCOUNT_ID)):
-                response = requests.post(f'https://api.tradier.com/v1/accounts/{TRADIER_ACCOUNT_ID[i]}/orders',
-                                        data = {'class': 'equity',
-                                                'symbol': f'{ticker}',
-                                                'side': f'{side}',
-                                                'quantity':  f'{qty}',
-                                                'type': 'market',
-                                                'duration': 'day'},
-                                        headers = {'Authorization': f'Bearer {TRADIER_ACCESS_TOKEN}',
-                                                   'Accept': 'application/json'}
-                                        )
-                if side == "buy":
-                    print(f"Bought {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
-                else:
-                    print(f"Sold {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
-    except:
-        return False
-    return True
-
+    if price is not None:
+        for i in range(len(TRADIER_ACCOUNT_ID)):
+            response = requests.post(f'https://api.tradier.com/v1/accounts/{TRADIER_ACCOUNT_ID[i]}/orders',
+                                    data = {'class': 'equity',
+                                            'symbol': f'{ticker}',
+                                            'side': f'{side}',
+                                            'quantity': f'{qty}',
+                                            'type': 'limit',
+                                            'duration': 'day',
+                                            'price': f'{price}'},
+                                    headers = {'Authorization': f'Bearer {TRADIER_ACCESS_TOKEN}',
+                                                'Accept': 'application/json'}
+                                    )
+            if side == "buy":
+                print(f"Bought {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
+            else:
+                print(f"Sold {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
+    else:
+        for i in range(len(TRADIER_ACCOUNT_ID)):
+            response = requests.post(f'https://api.tradier.com/v1/accounts/{TRADIER_ACCOUNT_ID[i]}/orders',
+                                    data = {'class': 'equity',
+                                            'symbol': f'{ticker}',
+                                            'side': f'{side}',
+                                            'quantity':  f'{qty}',
+                                            'type': 'market',
+                                            'duration': 'day'},
+                                    headers = {'Authorization': f'Bearer {TRADIER_ACCESS_TOKEN}',
+                                                'Accept': 'application/json'}
+                                    )
+            if side == "buy":
+                print(f"Bought {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
+            else:
+                print(f"Sold {ticker} on Tradier account {TRADIER_ACCOUNT_ID[i]}")
 
 async def stockTwitTrade(side, qty, ticker, price):
     STOCKTWITS_ACCESS_TOKEN = os.getenv("STOCKTWITS_ACCESS_TOKEN")
@@ -147,48 +133,41 @@ async def stockTwitTrade(side, qty, ticker, price):
         print("Missing StockTwits credentials, skipping")
         return None
     
-    try:
-        if price is not None:
-            response = requests.post('https://trade-api.stinvest.co/api/v1/trading/orders',
-                                    json = {'asset_class': 'equities',
-                                            'limit_price': f'{price}',
-                                            'order_type': 'limit',
-                                            'quantity': f'{qty}',
-                                            'symbol': f'{ticker}',
-                                            'time_in_force': 'DAY',
-                                            'transaction_type': f'{side}'},
-                                    headers = {'Authorization': f'Bearer {STOCKTWITS_ACCESS_TOKEN}',
-                                              'Accept': 'application/json'}
-                                    )
-            if response.status_code == 401:
-                raise Exception("StockTwits: 401 Unauthorized: Is your access token correct?")
-            if side == "buy":
-                print(f"Bought {ticker} on StockTwits")
-            else:
-                print(f"Sold {ticker} on StockTwits")
+    if price is not None:
+        response = requests.post('https://trade-api.stinvest.co/api/v1/trading/orders',
+                                json = {'asset_class': 'equities',
+                                        'limit_price': f'{price}',
+                                        'order_type': 'limit',
+                                        'quantity': f'{qty}',
+                                        'symbol': f'{ticker}',
+                                        'time_in_force': 'DAY',
+                                        'transaction_type': f'{side}'},
+                                headers = {'Authorization': f'Bearer {STOCKTWITS_ACCESS_TOKEN}',
+                                            'Accept': 'application/json'}
+                                )
+        if response.status_code == 401:
+            raise Exception("StockTwits: 401 Unauthorized: Is your access token correct?")
+        if side == "buy":
+            print(f"Bought {ticker} on StockTwits")
         else:
-            response = requests.post('https://trade-api.stinvest.co/api/v1/trading/orders',
-                                    json = {'asset_class': 'equities',
-                                            'order_type': 'market',
-                                            'quantity': f'{qty}',
-                                            'symbol': f'{ticker}',
-                                            'time_in_force': 'DAY',
-                                            'transaction_type': f'{side}'},
-                                    headers = {'Authorization': f'Bearer {STOCKTWITS_ACCESS_TOKEN}',
-                                               'Accept': 'application/json'}
-                                    )
-            if response.status_code == 401:
-                raise Exception("StockTwits: 401 Unauthorized: Is your access token correct?")
-            if side == "buy":
-                print(f"Bought {ticker} on StockTwits")
-            else:
-                print(f"Sold {ticker} on StockTwits")
-    except Exception as e:
-        print(e)
-        return False
-    except:
-        return False
-    return True
+            print(f"Sold {ticker} on StockTwits")
+    else:
+        response = requests.post('https://trade-api.stinvest.co/api/v1/trading/orders',
+                                json = {'asset_class': 'equities',
+                                        'order_type': 'market',
+                                        'quantity': f'{qty}',
+                                        'symbol': f'{ticker}',
+                                        'time_in_force': 'DAY',
+                                        'transaction_type': f'{side}'},
+                                headers = {'Authorization': f'Bearer {STOCKTWITS_ACCESS_TOKEN}',
+                                            'Accept': 'application/json'}
+                                )
+        if response.status_code == 401:
+            raise Exception("StockTwits: 401 Unauthorized: Is your access token correct?")
+        if side == "buy":
+            print(f"Bought {ticker} on StockTwits")
+        else:
+            print(f"Sold {ticker} on StockTwits")
 
 #TODO: Implement Webull Trading
 #def webullTrade():
